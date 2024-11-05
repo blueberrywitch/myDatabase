4a84b0819fef3e4e4054d748463d4a85cd16de80
DinaraM <blueberrywitch@yandex.ru>
2024-11-05 21:19:14 +0300

    first commit

---
+++ src/main/java/dika/mydatabase/MyDatabaseApplication.java
@@ -0,0 +1,13 @@
+package dika.mydatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dika/mydatabase/MyDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package dika.mydatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
